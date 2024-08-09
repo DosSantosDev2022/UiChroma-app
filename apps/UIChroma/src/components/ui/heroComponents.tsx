4fc0b3b6a926e391c012132d434520659c029f99
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-08-09 16:46:35 -0300

    styles: refatorando os estilos componente heroComponents

--- apps/UIChroma/src/components/ui/heroComponents.tsx
+++ apps/UIChroma/src/components/ui/heroComponents.tsx
@@ -1,20 +1,22 @@
+import { inter } from "@/assets/fonts"
+
 interface HeroComponentsProps {
-  content: string
+  type: string
   name: string | undefined
   description: string | undefined
 }
 
 export function HeroComponents({
-  content,
+  type,
   description,
   name,
 }: HeroComponentsProps) {
   return (
     <div className=" flex w-full flex-col">
-      <span className="bg-primary-950 text-secondary-50 w-40 rounded-lg px-4 py-2 text-center text-lg font-medium leading-[29px] ">
-        {content}
+      <span className="bg-primary-950 text-secondary-50 w-28 rounded-lg px-1.5 py-2 text-center text-base font-medium leading-[29px] ">
+        {type}
       </span>
-      <h1 className="text-primary-900 mt-6 text-5xl font-extrabold tracking-[2.16px] ">
+      <h1 className={`text-primary-900 mt-6 text-4xl font-extrabold ${inter.className}`} >
         {name}
       </h1>
       <p className="text-primary-800 mt-4 w-[500px] text-base font-normal ">

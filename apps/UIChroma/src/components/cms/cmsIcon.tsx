fec5ab487c853f3764c0d2c5a64f16dce79c86c0
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-08-09 16:49:00 -0300

    Feat: adicionando o componente cmsIcon

---
+++ apps/UIChroma/src/components/cms/cmsIcon.tsx
@@ -0,0 +1,15 @@
+type CMSIconProps = {
+  icon: string
+  className?: string
+}
+
+export function CMSIcon({ icon, className }: CMSIconProps) {
+  return (
+    <div
+      className={className}
+      dangerouslySetInnerHTML={{
+        __html: `<i class="${className}">${icon}</i>`,
+      }}
+    />
+  )
+}

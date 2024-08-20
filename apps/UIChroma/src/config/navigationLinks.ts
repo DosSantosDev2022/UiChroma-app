63c5220a4a9bf48012538091d15705ce72e8b11e
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-08-19 22:59:28 -0300

    FEAT: Criando arquivo para armazenar configurações reutilizáveis

---
+++ apps/UIChroma/src/config/navigationLinks.ts
@@ -0,0 +1,33 @@
+ const pagesectionlinks = [
+  {
+    text: 'Início',
+    url: 'inicio',
+  },
+  {
+    text: 'Features',
+    url: 'feature',
+  },
+  {
+    text: 'Preview',
+    url: 'preview',
+  },
+  {
+    text: 'CopyCode',
+    url: 'copyCode',
+  },
+  {
+    text: 'Dependencias',
+    url: 'dependencias',
+  },
+  {
+    text: 'Como usar',
+    url: 'como-usar',
+  },
+]
+
+
+
+
+export {
+  pagesectionlinks
+}

53224a51db4c2fd23133d3b9b40b525c3c839534
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-12-03 13:32:15 -0300

    CHORE: Altera font family do projeto

--- apps/UIChroma/src/app/layout.tsx
+++ apps/UIChroma/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 
-import { nunito } from '@/assets/fonts'
+import { inter } from '@/assets/fonts'
 import { Header } from '@/components/global/header/Header'
 import { SideBar } from '@/components/global/sideBar/SideBar'
 import '../../../../global.css'
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`${nunito.className} antialiased`}>
+      <body className={`${inter.className} antialiased`}>
         <div className="grid h-screen grid-cols-[260px_1fr] grid-rows-[auto_1fr] overflow-hidden">
           <SideBar />
 

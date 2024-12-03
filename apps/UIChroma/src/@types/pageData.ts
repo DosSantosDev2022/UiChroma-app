cd2b04e2139d1dd8959e8f4eddde803eed4ad177
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-12-03 13:30:15 -0300

    FEAT: Cria arquivo type global para query em paginas

---
+++ apps/UIChroma/src/@types/pageData.ts
@@ -0,0 +1,22 @@
+import { RichTextContent } from "@graphcms/rich-text-types";
+
+interface Section {
+  id: string;
+  title: string;
+  description: string;
+  content: {
+    raw: RichTextContent
+  }
+}
+
+export interface PageDataTypes {
+  documentationPage: {
+    id: string;
+    title: string;
+    slug: string;
+    section: Section | null;
+    section02: Section | null;
+    section03: Section | null;
+    section04: Section | null;
+  }
+}

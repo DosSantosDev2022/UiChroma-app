450ee2bb5b07de0f8039cf57329645bee15d9e1f
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-12-05 00:03:54 -0300

    FEAT: Alterações no tailwind.config para temas

--- apps/UIChroma/src/utils/generateCode.ts
+++ apps/UIChroma/src/utils/generateCode.ts
@@ -85,7 +85,7 @@ export const generateCodeCss = (colors: Colors) => {
 export const generateTailwindConfig = () => {
   return `
 
-    import type { Config } from 'tailwindcss'
+  import type { Config } from 'tailwindcss'
 
   const config: Config = {
     content: [
@@ -93,92 +93,48 @@ export const generateTailwindConfig = () => {
       './src/components/**/*.{js,ts,jsx,tsx,mdx}',
       './src/app/**/*.{js,ts,jsx,tsx,mdx}',
       ],
-      theme: {
-        extend: {
-          colors: {
-            background: 'hsl(var(--background))',
-            foreground: 'hsl(var(--foreground))',
-            card: 'hsl(var(--card))',
-            'card-foreground': 'hsl(var(--card-foreground))',
-            'popover: 'hsl(var(--popover))',
-            'popover-foreground': 'hsl(var(--popover-foreground))',
-            'primary: 'hsl(var(--primary))',
-            'primary-foreground': 'hsl(var(--primary-foreground))',
-            'secondary: 'hsl(var(--secondary))',
-            'secondary-foreground': 'hsl(var(--secondary-foreground))',
-            'muted: 'hsl(var(--muted))',
-            'muted-foreground': 'hsl(var(--muted-foreground))',
-            'accent: 'hsl(var(--accent))',
-            'accent-foreground': 'hsl(var(--accent-foreground))',
-            'destructive: 'hsl(var(--destructive))',
-            'destructive-foreground': 'hsl(var(--destructive-foreground))',
-            'border: 'hsl(var(--border))',
-            'input: 'hsl(var(--input))',
-            'ring: 'hsl(var(--ring))',
-            'chart-1': 'hsl(var(--chart-1))',
-            'chart-2': 'hsl(var(--chart-2))',
-            'chart-3': 'hsl(var(--chart-3))',
-            'chart-4': 'hsl(var(--chart-4))',
-            'chart-5': 'hsl(var(--chart-5))',
-            },
-            keyframes: {
-              'fade-in': {
-                '0%': { opacity: '0' },
-                '100%': { opacity: '1' },
-              },
-              'fade-out': {
-                '0%': { opacity: '1' },
-                '100%': { opacity: '0' },
-              },
-              'slide-in-from-top': {
-                '0%': { transform: 'translateY(-100%)' },
-                '100%': { transform: 'translateY(0)' },
-              },
-              'slide-out-to-top': {
-                '0%': { transform: 'translateY(0)' },
-                '100%': { transform: 'translateY(-100%)' },
-              },
-              'slide-in-from-bottom': {
-                '0%': { transform: 'translateY(100%)' },
-                '100%': { transform: 'translateY(0)' },
-              },
-              'slide-out-to-bottom': {
-                '0%': { transform: 'translateY(0)' },
-                '100%': { transform: 'translateY(100%)' },
-              },
-              'slide-in-from-left': {
-                '0%': { transform: 'translateX(-100%)' },
-                '100%': { transform: 'translateX(0)' },
-              },
-              'slide-out-to-left': {
-                '0%': { transform: 'translateX(0)' },
-                '100%': { transform: 'translateX(-100%)' },
-              },
-              'slide-in-from-right': {
-                '0%': { transform: 'translateX(100%)' },
-                '100%': { transform: 'translateX(0)' },
-              },
-              'slide-out-to-right': {
-                '0%': { transform: 'translateX(0)' },
-                '100%': { transform: 'translateX(100%)' },
-              },
-            },
-            animation: {
-              'fade-in': 'fade-in 0.5s ease-out forwards',
-              'fade-out': 'fade-out 0.5s ease-out forwards',
-              'slide-in-from-top': 'slide-in-from-top 0.5s ease-out forwards',
-              'slide-out-to-top': 'slide-out-to-top 0.5s ease-out forwards',
-              'slide-in-from-bottom': 'slide-in-from-bottom 0.5s ease-out forwards',
-              'slide-out-to-bottom': 'slide-out-to-bottom 0.5s ease-out forwards',
-              'slide-in-from-left': 'slide-in-from-left 0.5s ease-out forwards',
-              'slide-out-to-left': 'slide-out-to-left 0.5s ease-out forwards',
-              'slide-in-from-right': 'slide-in-from-right 0.5s ease-out forwards',
-              'slide-out-to-right': 'slide-out-to-right 0.5s ease-out forwards',
+        theme: {
+         extend: {
+            colors: {
+              background: 'hsl(var(--background))',
+              'background-hover': 'hsl(var(--background-hover))',
+              foreground: 'hsl(var(--foreground))',
+              'foreground-hover': 'hsl(var(--foreground-hover))',
+              primary: 'hsl(var(--primary))',
+              'primary-hover': 'hsl(var(--primary-hover))',
+              'primary-foreground': 'hsl(var(--primary-foreground))',
+              secondary: 'hsl(var(--secondary))',
+              'secondary-hover': 'hsl(var(--secondary-hover))',
+              'secondary-foreground': 'hsl(var(--secondary-foreground))',
+              muted: 'hsl(var(--muted))',
+              'muted-hover': 'hsl(var(--muted-hover))',
+              'muted-foreground': 'hsl(var(--muted-foreground))',
+              accent: 'hsl(var(--accent))',
+              'accent-hover': 'hsl(var(--accent-hover))',
+              'accent-foreground': 'hsl(var(--accent-foreground))',
+              destructive: 'hsl(var(--destructive))',
+              'destructive-hover': 'hsl(var(--destructive-hover))',
+              'destructive-foreground': 'hsl(var(--destructive-foreground))',
+              border: 'hsl(var(--border))',
+              'border-hover': 'hsl(var(--border-hover))',
+              input: 'hsl(var(--input))',
+              'input-hover': 'hsl(var(--input-hover))',
+              ring: 'hsl(var(--ring))',
+              'ring-hover': 'hsl(var(--ring-hover))',
+              'chart-1': 'hsl(var(--chart-1))',
+              'chart-1-hover': 'hsl(var(--chart-1-hover))',
+              'chart-2': 'hsl(var(--chart-2))',
+              'chart-2-hover': 'hsl(var(--chart-2-hover))',
+              'chart-3': 'hsl(var(--chart-3))',
+              'chart-3-hover': 'hsl(var(--chart-3-hover))',
+              'chart-4': 'hsl(var(--chart-4))',
+              'chart-4-hover': 'hsl(var(--chart-4-hover))',
+              'chart-5': 'hsl(var(--chart-5))',
+              'chart-5-hover': 'hsl(var(--chart-5-hover))',
             },
           },
         },
-      };
 
      export default config
-    `
+  `
 }

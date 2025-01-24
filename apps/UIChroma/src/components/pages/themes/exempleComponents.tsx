fe719464b175ef9d624b54a0b2e44eaa14f206c6
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-01-24 16:51:01 -0300

    fix(colorsTheme): Correção na logica do arquivo updateCssVariables
    Estavamos enfrentando problemas com a funcionalidade que alterava as cores de acordo com o tema selecionado nos componentes de exemplo, corrigi isso voltando a usar o updateCssVariables

--- apps/UIChroma/src/components/pages/themes/exempleComponents.tsx
+++ apps/UIChroma/src/components/pages/themes/exempleComponents.tsx
@@ -1,10 +1,19 @@
 'use client'
 import * as Examples from '@/components/@examples'
 import { ContainerPreview } from './container'
+import { useThemeStore } from '@/store/useThemeStore'
+import { useEffect } from 'react'
+import { updateCssVariables } from '@/utils/updateCssVariables'
 
 export function ExempleComponents() {
+  const { theme, lightColors, darkColors } = useThemeStore()
+
+  useEffect(() => {
+    updateCssVariables(theme, lightColors, darkColors)
+  }, [theme, lightColors, darkColors])
+
   return (
-    <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+    <div id="demo-container" className="grid grid-cols-1 gap-6 lg:grid-cols-2">
       {/* buttons preview */}
       <ContainerPreview title="Componente button">
         <Examples.ButtonPreview />
